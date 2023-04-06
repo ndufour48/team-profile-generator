eebcd77efb24b2b31b9ad2aec9c8bcc6a474d980
Nicholas DuFour <nick.dufour48@gmail.com>
2023-04-06 03:52:42 -0400

    bootstrap / starter html

--- index.js
+++ index.js
@@ -7,50 +7,75 @@ const inquirer = require("inquirer");
 const path = require("path");
 
 const questions = [
-    {
-      type: "input",
-      name: "managerName",
-      message: "Please enter managers name",
+  {
+    type: "input",
+    name: "managerName",
+    message: "Please enter managers name",
+  },
+  {
+    type: "input",
+    name: "mgrID",
+    message: "Please enter your ID number",
+  },
+  {
+    type: "input",
+    name: "mgrEmail",
+    message: "Please enter your email",
+  },
+  {
+    type: "input",
+    name: "offNum",
+    message: "Please enter your office phone number",
+  },
+  {
+    type: "list",
+    name: "addTeam",
+    message: "Would you like to add anyone to your team?",
+    choices: ["Engineer", "Intern", "No one else to add"],
+  },
+  {
+    type: "input",
+    name: "engName",
+    message: "Please enter your engineers name",
+    when: function (answers) {
+      return answers.addTeam === "Engineer";
     },
-    {
-      type: "input",
-      name: "mgrID",
-      message: "Please enter your ID number",
+  },
+  {
+    type: "input",
+    name: "engID",
+    message: "Please enter your engineers ID number",
+  },
+  {
+    type: "input",
+    name: "engEmail",
+    message: "Please enter your engineers email",
+  },
+  {
+    type: "input",
+    name: "gitHub",
+    message: "Please enter your engineers GitHub username",
+  },
+  {
+    type: "list",
+    name: "addTeam2",
+    message: "Would you like to add anyone else to your team?",
+    choices: ["Engineer", "Intern", "No one else to add"],
+    when: function (answers) {
+      return answers.gitHub === true;
     },
-    {
-      type: "input",
-      name: "mgrEmail",
-      message: "Please enter your email",
-    },
-    {
-      type: "input",
-      name: "offNum",
-      message: "Please enter your office phone number",
-    },
-    {
-      type: "list",
-      name: "addTeam",
-      message: "Would you like to add anyone to your team?",
-      choices: ["Engineer", "Intern", "No one else to add"],
-    },
-    {
-      type: "input",
-      name: "engName",
-      message: "Please enter your engineers name",
-      when: function (answers) {
-        return answers.addTeam === "Engineer";
-      },
-    },
-  ];
-  
-  function init() {
-    inquirer
-      .prompt(questions)
-      .then((data) => {
-        console.log(data);
-      })
-      .catch((err) => console.error(err));
-  }
-  
-  // Function call to initialize app
-  init();
+  },
+];
+
+function init() {
+  console.log("Please build your team");
+  inquirer
+    .prompt(questions)
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((err) => console.error(err));
+}
+
+// Function call to initialize app
+init();
